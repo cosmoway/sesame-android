cce8485265bc36e9cf0413126a8a73cf992cefb5
ANDO <ando@cosmoway.net>
2016-02-26 18:11:26 +0900

    BootstrapNotifierインターフェースを実装。

--- app/src/main/kotlin/net/cosmoway/sesame/BeaconApplication.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/BeaconApplication.kt
@@ -1,6 +1,8 @@
 package net.cosmoway.sesame
 
 import android.app.Application
+import org.altbeacon.beacon.Region
+import org.altbeacon.beacon.startup.BootstrapNotifier
 
 import org.altbeacon.beacon.startup.RegionBootstrap
 
@@ -8,16 +10,28 @@ import org.altbeacon.beacon.startup.RegionBootstrap
  * Created by ando on 16/02/26.
  * a
  */
-class BeaconApplication : Application() {
+class BeaconApplication : Application(), BootstrapNotifier {
 
-    private val regionBootstrap: RegionBootstrap? = null
+    private var regionBootstrap: RegionBootstrap? = null
+
+    companion object {
+
+        val TAG = BeaconApplication::class.java.simpleName
+    }
 
     override fun onCreate() {
         super.onCreate()
     }
 
-    companion object {
+    override fun didEnterRegion(p0: Region?) {
+        // 領域に入場した
+    }
 
-        val TAG = BeaconApplication::class.java.simpleName
+    override fun didExitRegion(p0: Region?) {
+        // 領域から退場した
+    }
+
+    override fun didDetermineStateForRegion(p0: Int, p1: Region?) {
+        // 入退場状態が変更された
     }
 }
