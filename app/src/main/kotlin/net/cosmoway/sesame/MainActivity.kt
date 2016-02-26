6388873ea4df208b0b52e75f2e7ef9453782613c
ANDO <ando@cosmoway.net>
2016-02-26 17:41:48 +0900

    領域監視の開始。

--- app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
@@ -2,13 +2,16 @@ package net.cosmoway.sesame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.RemoteException
 import org.altbeacon.beacon.BeaconConsumer
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.Region
 
 class MainActivity : AppCompatActivity(), BeaconConsumer {
 
     private var mBeaconManager: BeaconManager? = null
+    private var mRegion: Region? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +23,17 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
         val IBEACON_FORMAT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
         mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
 
-
+        //とりあえずbeacon全部認識するように
+        mRegion = Region("unique-id-001", null, null, null)
     }
 
     override fun onBeaconServiceConnect() {
-        throw UnsupportedOperationException()
+        try {
+            // ビーコン情報の監視を開始
+            mBeaconManager?.startMonitoringBeaconsInRegion(mRegion)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
     }
 
     override fun onResume() {
