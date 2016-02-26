a55aa25092c4ac884f2b5ce94292ee14365bc5f4
ANDO <ando@cosmoway.net>
2016-02-26 17:27:09 +0900

    Bluetoothマネージャのインスタンス化。

--- app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
@@ -2,11 +2,16 @@ package net.cosmoway.sesame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.altbeacon.beacon.BeaconManager
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBeaconManager: BeaconManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //BTMのインスタンス化
+        mBeaconManager = BeaconManager.getInstanceForApplication(this)
     }
 }
