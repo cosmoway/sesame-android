bc2ed3c91ff1797e64d2df124a62ae1cbbbe9c70
ANDO <ando@cosmoway.net>
2016-02-26 18:20:16 +0900

    バックグラウンドの領域監視を開始。

--- app/src/main/kotlin/net/cosmoway/sesame/BeaconApplication.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/BeaconApplication.kt
@@ -2,6 +2,7 @@ package net.cosmoway.sesame
 
 import android.app.Application
 import android.content.Intent
+import android.util.Log
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
 import org.altbeacon.beacon.Region
@@ -30,20 +31,27 @@ class BeaconApplication : Application(), BootstrapNotifier {
         //Parserの設定
         val IBEACON_FORMAT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
         mBeaconManager?.beaconParsers?.add(BeaconParser().setBeaconLayout(IBEACON_FORMAT))
+
+        // UUID, major, minorの指定はしない
+        var region: Region = Region("uuid-region-bootstrap-001", null, null, null)
+        regionBootstrap = RegionBootstrap(this, region)
     }
 
-    override fun didEnterRegion(p0: Region?) {
+    override fun didEnterRegion(region: Region?) {
         // 領域に入場した
+        Log.d(TAG, "Enter Region")
         var intent: Intent = Intent(this, MainActivity::class.java)
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
     }
 
-    override fun didExitRegion(p0: Region?) {
+    override fun didExitRegion(region: Region?) {
         // 領域から退場した
+        Log.d(TAG, "Exit")
     }
 
-    override fun didDetermineStateForRegion(p0: Int, p1: Region?) {
+    override fun didDetermineStateForRegion(i: Int, region: Region?) {
         // 入退場状態が変更された
+        Log.d(TAG, "State:" + i)
     }
 }
