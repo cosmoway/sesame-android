1ca4f4a19d2d970602daf322920c9bfae79d082d
ANDO <ando@cosmoway.net>
2016-03-01 19:02:36 +0900

    領域進入時に画面が起きる。

--- app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
@@ -3,9 +3,13 @@ package net.cosmoway.sesame
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 
 
 class MainActivity : Activity() {
+
+    //スリープモードからの復帰の為のフラグ定数
+    private val FLAG_KEYGUARD = WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,11 +18,13 @@ class MainActivity : Activity() {
         startService(Intent(this, SesameBeaconService::class.java))
     }
 
-    override fun onPause() {
-        super.onPause()
+    override fun onStop() {
+        super.onStop()
+        window.clearFlags(FLAG_KEYGUARD)
     }
 
     override fun onResume() {
         super.onResume()
+        window.addFlags(FLAG_KEYGUARD)
     }
 }
