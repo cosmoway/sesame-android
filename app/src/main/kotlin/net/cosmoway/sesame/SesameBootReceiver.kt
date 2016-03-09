d13a543c91bfe121587692e4d129463111122b2b
ANDO <ando@cosmoway.net>
2016-03-09 17:32:52 +0900

    kotlinにコンバート。

---
+++ app/src/main/kotlin/net/cosmoway/sesame/SesameBootReceiver.kt
@@ -0,0 +1,14 @@
+package net.cosmoway.sesame
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SesameBootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            context.startService(Intent(context, SesameBeaconService::class.java))
+        }
+    }
+}
