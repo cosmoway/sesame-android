1779c48d2decd3d0d54f47f6c7b2d66027e76ad4
ANDO <ando@cosmoway.net>
2016-03-11 08:45:50 +0900

    再度、アプリの常駐化。

---
+++ app/src/main/kotlin/net/cosmoway/sesame/n.java
@@ -0,0 +1,13 @@
+package net.cosmoway.sesame;
+
+import org.altbeacon.beacon.BeaconManager;
+
+/**
+ * Created by ando on 16/03/11.
+ */
+public class n {
+    BeaconManager mBeaconManager;
+    public n(){
+        mBeaconManager.setAndroidLScanningDisabled(true);
+    }
+}
