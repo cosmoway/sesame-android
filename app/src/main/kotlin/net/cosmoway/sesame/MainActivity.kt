2f7056e2da4c41380fe274162b3cad665335fbb8
ANDO <ando@cosmoway.net>
2016-03-11 19:55:00 +0900

    Dozeモード無視要求の追加。

--- app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
@@ -4,15 +4,17 @@ import android.Manifest
 import android.app.ListActivity
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.net.wifi.WifiManager
-import android.os.Bundle
-import android.os.Handler
-import android.os.Message
+import android.os.*
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -24,6 +26,7 @@ class MainActivity : ListActivity() {
     private val FLAG_KEYGUARD = WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
     private var mReceiver: SesameBroadcastReceiver? = null
     private var mIntentFilter: IntentFilter? = null
+    private val REQUEST_PERMISSION: Int = 1;
 
     // サービスから値を受け取ったら動かしたい内容を書く
     private val updateHandler = object : Handler() {
@@ -50,6 +53,24 @@ class MainActivity : ListActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //電源に関するパーミッション要求。
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) run {
+            val pm = getSystemService(POWER_SERVICE) as PowerManager
+            val packageName = packageName
+            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+                AlertDialog.Builder(this)
+                        .setTitle("確認")
+                        .setMessage("本アプリが正常に動作する為には、電池の最適化の解除が必要です。"
+                                + "\nなお、最適化状態時は、ビーコンの監視に影響が発生します。")
+                        .setNeutralButton("OK") { dialog, which ->
+                            val intent = Intent(
+                                    Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                            intent.data = Uri.parse("package:" + packageName)
+                            startActivityForResult(intent, REQUEST_PERMISSION)
+                        }.show()
+            }
+        }
 
         requestLocationPermission()
 
