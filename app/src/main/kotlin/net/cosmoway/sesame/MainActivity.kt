353119f95f296658240628a7f3ee6fcf0787a6fd
ANDO <ando@cosmoway.net>
2016-04-06 19:02:30 +0900

    テスト用にサービス開始/停止ボタンを付けました。

--- app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
+++ app/src/main/kotlin/net/cosmoway/sesame/MainActivity.kt
@@ -10,18 +10,23 @@ import android.os.Handler
 import android.os.Message
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.view.View
 import android.view.WindowManager
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 
 
-class MainActivity : ListActivity() {
+class MainActivity : ListActivity(), View.OnClickListener {
+    private var mReceiver: SesameBroadcastReceiver? = null
+    private var mIntentFilter: IntentFilter? = null
+    private var mStartButton: Button? = null
+    private var mStopButton: Button? = null
 
     //スリープモードからの復帰の為のフラグ定数
     private val FLAG_KEYGUARD = WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-    private var mReceiver: SesameBroadcastReceiver? = null
-    private var mIntentFilter: IntentFilter? = null
+
 
     // サービスから値を受け取ったら動かしたい内容を書く
     private val updateHandler = object : Handler() {
@@ -62,6 +67,19 @@ class MainActivity : ListActivity() {
         registerReceiver(mReceiver, mIntentFilter)
 
         (mReceiver as SesameBroadcastReceiver).registerHandler(updateHandler)
+
+        mStartButton = findViewById(R.id.btn_connect) as Button
+        mStopButton = findViewById(R.id.btn_disconnect) as Button
+        (mStartButton as Button).setOnClickListener(this)
+        (mStopButton as Button).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v == mStartButton) {
+            startService(Intent(this@MainActivity, SesameBeaconService::class.java))
+        } else if (v == mStopButton) {
+            stopService(Intent(this@MainActivity, SesameBeaconService::class.java))
+        }
     }
 
     override fun onStop() {
@@ -80,7 +98,7 @@ class MainActivity : ListActivity() {
             return
         }
 
-        startService(Intent(this, SesameBeaconService::class.java))
+        //startService(Intent(this, SesameBeaconService::class.java))
     }
 
     override fun onDestroy() {
